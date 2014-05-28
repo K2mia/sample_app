e63968fdd9b2a4b2af310551ee96865f2872edf8
Andrew Cowan <icculus@gmdstudios.com>
2014-05-28 10:01:11 -0400

    Initial commit for Sample App for Rails Tutorial Book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b691461321e58f0720b4725cc6744f14ca1e1c2d261eee73c90acdf1e8d851265cac3eb1d1a1c6805f2d353175d2a8289251172aeed15bbcae02da349d2c1c27'
